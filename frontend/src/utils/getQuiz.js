8895eacad75997d8a6071efb8e795c337afcf14d
ahmetys <ahmetyurtsevergm@gmail.com>
2024-05-25 23:27:55 +0200

    jkh

---
+++ frontend/src/utils/getQuiz.js
@@ -0,0 +1,25 @@
+const getRandomIndex = (array) => (Array.isArray(array) ? Math.floor(Math.random() * (array.length - 1)) : new Error("is not an array"));
+const getQuiz = (array, limit) => {
+  let quiz = [];
+  let filteredArray = array;
+
+  const createQuestion = () => {
+    const questionIndex = getRandomIndex(filteredArray);
+    const options = () => {
+      let options = [];
+      options = [filteredArray[getRandomIndex(filteredArray)].target, filteredArray[getRandomIndex(filteredArray)].target, filteredArray[getRandomIndex(filteredArray)].target, filteredArray[getRandomIndex(filteredArray)].target];
+      options[getRandomIndex(options)] = array[questionIndex].target;
+      return options;
+    };
+    const question = { question: filteredArray[questionIndex].source, answer: filteredArray[questionIndex].target, options: options() };
+    filteredArray = filteredArray.filter((q, i) => i !== questionIndex);
+    console.log(filteredArray);
+    quiz = [...quiz, question];
+    quiz.length < limit && createQuestion();
+  };
+  createQuestion();
+  console.log(quiz);
+  return quiz;
+};
+
+export { getQuiz };
